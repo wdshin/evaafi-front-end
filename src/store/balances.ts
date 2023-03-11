7684a5b75bd12c7121b3c36c222e66f883bf0bfa
Sepezho <sepezho@gmail.com>
2023-03-11 12:27:10 +0400

    main sc parser

--- src/store/balances.ts
+++ src/store/balances.ts
@@ -10,6 +10,18 @@ export const toncenter = new TonClient({
 
 export const masterContractAddress = Address.parse('EQBppIY6_jeZOmkYjdAmhHsO3YRPWC5vA6LOdRy4OkhhpeIS');
 
+function bufferToBigInt(buffer: any, start = 0, end = buffer.length) {
+    const bufferAsHexString = buffer.slice(start, end).toString("hex");
+    return BigInt(`0x${bufferAsHexString}`);
+}
+
+function hex2a(hexx: any) {
+    var hex = hexx.toString();//force conversion
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+}
 
 interface MySupply {
     id: string;
@@ -63,8 +75,8 @@ export const useBalance = create<BalanceStore>((set) => {
             'getUIVariables',
             args.build(),
         );
-        console.log('1---------------')
 
+        console.log('1---------------')
         stack.readCell().beginParse(); // important 
 
         const dict = Dictionary.loadDirect(Dictionary.Keys.BigUint(256), {
@@ -72,19 +84,12 @@ export const useBalance = create<BalanceStore>((set) => {
                 buidler.storeSlice(src);
             },
             parse: (src: Slice) => {
-                // ;;		.store_int(price, 64)
-                // ;;		.store_int(s_rate, 64)
-                // ;;		.store_int(b_rate, 64)
-                // ;;		.store_int(total_supply_principal, 64)
-                // ;;		.store_int(total_borrow_principal, 64)
-                // ;;		.store_int(last_accural, 64)
-
-                const price = src.loadUint(64);                       // price
-                const s_rate = src.loadUint(64);                      // s_rate
-                const b_rate = src.loadUint(64);                      // b_rate
-                const totalSupply = fromNano(src.loadUint(64));  // total_supply
-                const totalBorrow = fromNano(src.loadUint(64));  // total_borrow
-                const lastAccural = src.loadUint(64);
+                const price = BigInt(src.loadUint(64));                       // price
+                const s_rate = BigInt(src.loadUint(64));                      // s_rate
+                const b_rate = BigInt(src.loadUint(64));                      // b_rate
+                const totalSupply = BigInt(src.loadUint(64));  // total_supply
+                const totalBorrow = BigInt(src.loadUint(64));  // total_borrow
+                const lastAccural = BigInt(src.loadUint(64));
                 const balance = 100;               // last_accural
 
                 return { price, s_rate, b_rate, totalSupply, totalBorrow, lastAccural, balance };
@@ -92,10 +97,6 @@ export const useBalance = create<BalanceStore>((set) => {
             //@ts-ignore
         }, stack.readCellOpt())
 
-        function bufferToBigInt(buffer: any, start = 0, end = buffer.length) {
-            const bufferAsHexString = buffer.slice(start, end).toString("hex");
-            return BigInt(`0x${bufferAsHexString}`);
-        }
         let data = dict.get(bufferToBigInt(randomAddress('usdt').hash))
 
         console.log(data, " usdt");
@@ -106,26 +107,15 @@ export const useBalance = create<BalanceStore>((set) => {
         // const borrowBalance = data.totalBorrow;
         // set({ borrowBalance });
 
-        console.log(bufferToBigInt(randomAddress('usdt').hash))
-        console.log(dict.keys())
-        console.log(dict.values())
-
-
         console.log('2---------------')
-        function hex2a(hexx: any) {
-            var hex = hexx.toString();//force conversion
-            var str = '';
-            for (var i = 0; i < hex.length; i += 2)
-                str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-            return str;
-        }
-
-
 
         //@ts-ignore
         const conf = stack.pop().cell.beginParse()
-        console.log(hex2a(conf.loadRef().beginParse().toString().slice(2).slice(0, -1)))
+        const metadata = hex2a(conf.loadRef().beginParse().toString().slice(2).slice(0, -1))
+        console.log(metadata)
+
         console.log('3--------------')
+
         conf.loadRef()
         const confItems = conf.loadRef().beginParse()
 
@@ -135,17 +125,17 @@ export const useBalance = create<BalanceStore>((set) => {
             },
             parse: (src: Slice) => {
                 const oracle = src.loadAddress();             //store_slice(oracle)
-                const decimals = src.loadUint(8);             //.store_uint(decimals, 8)
+                const decimals = BigInt(src.loadUint(8));             //.store_uint(decimals, 8)
                 const ref = src.loadRef().beginParse();       //.store_ref(begin_cell()
-                const collateralFactor = ref.loadUint(16);    //.store_uint(collateral_factor, 16) 
-                const liquidationThreshold = ref.loadUint(16);//.store_uint(liquidation_threshold, 16) 
-                const liquidationPenalty = ref.loadUint(16);  // .store_uint(liquidation_penalty, 16)
-                const baseBorrowRate = ref.loadUint(64);      //.store_uint(base_borrow_rate, 64) 
-                const borrowRateSlopeLow = ref.loadUint(64);  //.store_uint(borrow_rate_slope_low, 64) 
-                const borrowRateSlopeHigh = ref.loadUint(64); //.store_uint(supply_rate_slope_low, 64) 
-                const supplyRateSlopeLow = ref.loadUint(64);  //.store_uint(supply_rate_slope_low, 64) 
-                const supplyRateSlopeHigh = ref.loadUint(64); //.store_uint(supply_rate_slope_high, 64) 
-                const targeUtilization = ref.loadUint(64);    //.store_uint(target_utilization, 64) 
+                const collateralFactor = BigInt(ref.loadUint(16));    //.store_uint(collateral_factor, 16) 
+                const liquidationThreshold = BigInt(ref.loadUint(16));//.store_uint(liquidation_threshold, 16) 
+                const liquidationPenalty = BigInt(ref.loadUint(16));  // .store_uint(liquidation_penalty, 16)
+                const baseBorrowRate = BigInt(ref.loadUint(64));      //.store_uint(base_borrow_rate, 64) 
+                const borrowRateSlopeLow = BigInt(ref.loadUint(64));  //.store_uint(borrow_rate_slope_low, 64) 
+                const borrowRateSlopeHigh = BigInt(ref.loadUint(64)); //.store_uint(supply_rate_slope_low, 64) 
+                const supplyRateSlopeLow = BigInt(ref.loadUint(64));  //.store_uint(supply_rate_slope_low, 64) 
+                const supplyRateSlopeHigh = BigInt(ref.loadUint(64)); //.store_uint(supply_rate_slope_high, 64) 
+                const targeUtilization = BigInt(ref.loadUint(64));    //.store_uint(target_utilization, 64) 
 
                 return {
                     oracle, decimals, collateralFactor, liquidationThreshold,
@@ -155,42 +145,38 @@ export const useBalance = create<BalanceStore>((set) => {
             }
             //@ts-ignore
         }, confItems.loadRef().beginParse())
-        // get asset config by add -------
+
+        // get asset config by address -------
         console.log(dictConf.get(bufferToBigInt(randomAddress('usdt').hash)))
         console.log('4---------------')
-        console.log(confItems.loadUint(8)) //if active = -1 (true) / 0 (false)
+        console.log(confItems.loadUint(8)) //if pool active = -1 (true) / 0 (false)
         console.log('5---------------')
-
-
-       //@ts-ignore
-        const rates = stack.pop().cell.beginParse()
-        // console.log(hex2a(rates.loadRef().beginParse().toString().slice(2).slice(0, -1)))
-        // rates.loadRef()
-        const ratesItems = rates.loadRef().beginParse()
 
         const dictRates = Dictionary.loadDirect(Dictionary.Keys.BigUint(256), {
             serialize: (src: any, buidler: any) => {
                 buidler.storeSlice(src);
             },
             parse: (src: Slice) => {
-
-                const s_ratePerSecond = src.loadUint(64); //s_rate_per_second 64bit
-                const b_ratePerSecond = src.loadUint(64); //b_rate_per_second 64bit
-
+                const s_ratePerSecond = BigInt(src.loadUint(64)); //s_rate_per_second 64bit
+                const b_ratePerSecond = BigInt(src.loadUint(64)); //b_rate_per_second 64bit
                 return { s_ratePerSecond, b_ratePerSecond };
             }
-            //@ts-ignore
-        }, ratesItems.loadRef().beginParse())
-        
+        }, stack.readCellOpt())
+
         console.log(dictRates.get(bufferToBigInt(randomAddress('usdt').hash)))
 
-        // console.log('7---------------')
-        // console.log(ratesItems.loadUint(8)) //if active = -1 (true) / 0 (false)
-        // console.log('8---------------')
+        console.log('6---------------')
+        const dictReserves = Dictionary.loadDirect(Dictionary.Keys.BigUint(256), {
+            serialize: (src: any, buidler: any) => {
+                buidler.storeSlice(src);
+            },
+            parse: (src: Slice) => {
+                const reserve = BigInt(src.loadUint(64)); //s_rate_per_second 64bit
+                return { reserve };
+            }
+        }, stack.readCellOpt())
 
-
-        //reserves
-        /** - reserve 64bit */
+        console.log(dictReserves.get(bufferToBigInt(randomAddress('usdt').hash)))
     }
 
     setInterval(updateData, 50000);
